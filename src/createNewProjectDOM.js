79ddcdf6c8dbb20e56ec7f9de7b24bfcc625c469
Robert Alex Barbu <robertalexbarbu@gmail.com>
2023-02-10 20:33:04 +0200

    Improve create project functionality
    Now users won't be allowed to use only white-spaces or already
    existing names when they create a new project

--- src/createNewProjectDOM.js
+++ src/createNewProjectDOM.js
@@ -1,13 +1,18 @@
 import Project from "./Project";
 
 export default function createNewProjectDOM() {
+
   /* CREATE NEW PROJECT FUNCTIONALITY */
   const newProjectModular = document.querySelector(".create-project-modular");
+
+  /* the form control */
+  const inputNewProjectName = document.querySelector("#project-name-input");
 
   /* Open */
   const createNewProjectIcon = document.querySelector(".create-project");
   createNewProjectIcon.addEventListener("click", () => {
     newProjectModular.classList.add("visible");
+    inputNewProjectName.value = "";
   });
 
   /* Close */
@@ -30,17 +35,35 @@ export default function createNewProjectDOM() {
   });
 
   /* Create */
-  const inputNewProjectName = document.querySelector("#project-name-input");
+  const errorNewProject = document.querySelector('.error');
+  let invalid = false;
+  inputNewProjectName.addEventListener('input', (event)=>{
+    invalid = false;
+    errorNewProject.textContent = "";
+    for(const current of Project.projects) {
+        if(current.name === event.target.value) {
+            errorNewProject.textContent = "Project already exists!";
+            invalid = true;
+            break;
+        }
+    }
+    const regex = /^\s+$/;
+    if(regex.test(event.target.value) === true) {
+        errorNewProject.textContent = "Invalid project name!";
+        invalid = true;
+    }
+  })
   const createNewProjectBtn = document.querySelector("#create-new-btn");
   createNewProjectBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    if (inputNewProjectName.reportValidity() === true) {
+    if (inputNewProjectName.reportValidity() === true && invalid === false) {
       /* eslint-disable no-new */
       new Project(inputNewProjectName.value);
       Project.storeLocal();
       localStorage.setItem("active", "true");
       Project.render();
       newProjectModular.classList.remove("visible");
+      console.log(inputNewProjectName);
     }
   });
 }
